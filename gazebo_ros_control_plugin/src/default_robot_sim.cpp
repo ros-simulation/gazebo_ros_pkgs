93e61d75ae5e2623d3398fc9765c565a03e65c75
Jonathan Bohren <jbo@jhu.edu>
2013-06-24 00:36:17 -0400

    updating default robot sim

--- gazebo_ros_control_plugin/src/default_robot_sim.cpp
+++ gazebo_ros_control_plugin/src/default_robot_sim.cpp
@@ -37,8 +37,8 @@
    Desc:   Hardware Interface for any simulated robot in Gazebo
 */
 
-#ifndef __GAZEBO_ROS_CONTROL_HW_
-#define __GAZEBO_ROS_CONTROL_HW_
+#ifndef __GAZEBO_ROS_CONTROL_PLUGIN_DEFAULT_ROBOT_SIM_H_
+#define __GAZEBO_ROS_CONTROL_PLUGIN_DEFAULT_ROBOT_SIM_H_
 
 // ros_control
 #include <hardware_interface/joint_command_interface.h>
@@ -53,108 +53,118 @@
 #include <ros/ros.h>
 #include <angles/angles.h>
 
+// ros_control + gazebo
+#include <gazebo_ros_control_sim_interface/robot_sim.h>
+
 namespace gazebo_ros_control
 {
 
-class GazeboRobotHW : public hardware_interface::RobotHW
-{
-public:
-  GazeboRobotHW(const std::vector<std::string>& joint_names) :
-    joint_names_(joint_names)
+  class DefaultRobotSim : public gazebo_ros_control_sim_interface::RobotSim
   {
-    // Resize vectors to our DOF
-    n_dof_ = joint_names_.size();
-    joint_position_.resize(n_dof_);
-    joint_velocity_.resize(n_dof_);
-    joint_effort_.resize(n_dof_);
-    joint_effort_command_.resize(n_dof_);
-    joint_velocity_command_.resize(n_dof_);
+  public:
 
-    // Initialize values
-    for(unsigned int j=0; j < n_dof_; j++)
+    bool initSim(
+        ros::NodeHandle model_nh, 
+        gazebo::physics::ModelPtr parent_model,
+        std::vector<std::string> joint_names) 
     {
-      joint_position_[j] = 1.0;
-      joint_velocity_[j] = 0.0;
-      joint_effort_[j] = 1.0;  // N/m for continuous joints
-      joint_effort_command_[j] = 0.0;
-      joint_velocity_command_[j] = 0.0;
+      // Store the joint names
+      joint_names_ = joint_names;
 
-      // Register joints
-      js_interface_.registerHandle(hardware_interface::JointStateHandle(
-          joint_names_[j], &joint_position_[j],&joint_velocity_[j], &joint_effort_[j]));          
-      ej_interface_.registerHandle(hardware_interface::JointHandle(
-          js_interface_.getHandle(joint_names_[j]),&joint_effort_command_[j]));          
-      vj_interface_.registerHandle(hardware_interface::JointHandle(
-          js_interface_.getHandle(joint_names_[j]),&joint_velocity_command_[j]));          
-    }
+      // Resize vectors to our DOF
+      n_dof_ = joint_names_.size();
+      joint_position_.resize(n_dof_);
+      joint_velocity_.resize(n_dof_);
+      joint_effort_.resize(n_dof_);
+      joint_effort_command_.resize(n_dof_);
+      joint_velocity_command_.resize(n_dof_);
 
-    // Register interfaces
-    registerInterface(&js_interface_);
-    registerInterface(&ej_interface_);
-    registerInterface(&vj_interface_);
-  }
-
-  bool initSim(gazebo::physics::ModelPtr model)
-  {
-    // Get the gazebo joints that correspond to the robot joints
-    for(unsigned int j=0; j < n_dof_; j++)
-    {
-      ROS_DEBUG_STREAM("Getting pointer to gazebo joint: "<<joint_names_[j]);
-      gazebo::physics::JointPtr joint = model->GetJoint(joint_names_[j]);
-      if (joint)
+      // Initialize values
+      for(unsigned int j=0; j < n_dof_; j++)
       {
-        sim_joints_.push_back(joint);
+        joint_position_[j] = 1.0;
+        joint_velocity_[j] = 0.0;
+        joint_effort_[j] = 1.0;  // N/m for continuous joints
+        joint_effort_command_[j] = 0.0;
+        joint_velocity_command_[j] = 0.0;
+
+        // Register joints
+        js_interface_.registerHandle(hardware_interface::JointStateHandle(
+                joint_names_[j], &joint_position_[j],&joint_velocity_[j], &joint_effort_[j]));          
+        ej_interface_.registerHandle(hardware_interface::JointHandle(
+                js_interface_.getHandle(joint_names_[j]),&joint_effort_command_[j]));          
+        vj_interface_.registerHandle(hardware_interface::JointHandle(
+                js_interface_.getHandle(joint_names_[j]),&joint_velocity_command_[j]));          
       }
-      else
+
+      // Register interfaces
+      registerInterface(&js_interface_);
+      registerInterface(&ej_interface_);
+      registerInterface(&vj_interface_);
+      // Get the gazebo joints that correspond to the robot joints
+      for(unsigned int j=0; j < n_dof_; j++)
       {
-        ROS_ERROR_STREAM("This robot has a joint named \""<<joint_names_[j]<<"\" which is not in the gazebo model.");
-        return false;
+        ROS_DEBUG_STREAM("Getting pointer to gazebo joint: "<<joint_names_[j]);
+        gazebo::physics::JointPtr joint = model->GetJoint(joint_names_[j]);
+        if (joint)
+        {
+          sim_joints_.push_back(joint);
+        }
+        else
+        {
+          ROS_ERROR_STREAM("This robot has a joint named \""<<joint_names_[j]<<"\" which is not in the gazebo model.");
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    void readSim(ros::Time time, ros::Duration period)
+    {
+      for(unsigned int j=0; j < n_dof_; j++)
+      {
+        // Gazebo has an interesting API...
+        joint_position_[j] += angles::shortest_angular_distance(joint_position_[j], 
+                                                                sim_joints_[j]->GetAngle(0).Radian());
+        joint_velocity_[j] = sim_joints_[j]->GetVelocity(0);
+        joint_effort_[j] = sim_joints_[j]->GetForce((unsigned int)(0));
       }
     }
 
-    return true;
-  }
-
-  void readSim(ros::Time time, ros::Duration period)
-  {
-    for(unsigned int j=0; j < n_dof_; j++)
+    void writeSim(ros::Time time, ros::Duration period)
     {
-      // Gazebo has an interesting API...
-      joint_position_[j] += angles::shortest_angular_distance(joint_position_[j], 
-                            sim_joints_[j]->GetAngle(0).Radian());
-      joint_velocity_[j] = sim_joints_[j]->GetVelocity(0);
-      joint_effort_[j] = sim_joints_[j]->GetForce((unsigned int)(0));
+      for(unsigned int j=0; j < n_dof_; j++)
+      {
+        // Gazebo has an interesting API...
+        sim_joints_[j]->SetForce(0,joint_effort_command_[j]);
+      }
     }
-  }
 
-  void writeSim(ros::Time time, ros::Duration period)
-  {
-    for(unsigned int j=0; j < n_dof_; j++)
-    {
-      // Gazebo has an interesting API...
-      sim_joints_[j]->SetForce(0,joint_effort_command_[j]);
-    }
-  }
+  private:
+    unsigned int n_dof_;
 
-private:
-  unsigned int n_dof_;
+    hardware_interface::JointStateInterface    js_interface_;
+    hardware_interface::EffortJointInterface   ej_interface_;
+    hardware_interface::VelocityJointInterface vj_interface_;
 
-  hardware_interface::JointStateInterface    js_interface_;
-  hardware_interface::EffortJointInterface   ej_interface_;
-  hardware_interface::VelocityJointInterface vj_interface_;
+    std::vector<std::string> joint_names_;
+    std::vector<double> joint_position_;
+    std::vector<double> joint_velocity_;
+    std::vector<double> joint_effort_;
+    std::vector<double> joint_effort_command_;
+    std::vector<double> joint_velocity_command_;
 
-  std::vector<std::string> joint_names_;
-  std::vector<double> joint_position_;
-  std::vector<double> joint_velocity_;
-  std::vector<double> joint_effort_;
-  std::vector<double> joint_effort_command_;
-  std::vector<double> joint_velocity_command_;
+    std::vector<gazebo::physics::JointPtr> sim_joints_;
+  };
 
-  std::vector<gazebo::physics::JointPtr> sim_joints_;
-};
-
-typedef boost::shared_ptr<GazeboRobotHW> GazeboRobotHWPtr;
+  typedef boost::shared_ptr<DefaultRobotSim> DefaultRobotSimPtr;
 
 }
 
-#endif // #ifndef __GAZEBO_ROS_CONTROL_HW_
+PLUGINLIB_DECLARE_CLASS(gazebo_ros_control_plugin,
+                        DefaultRobotSim,
+                        gazebo_ros_control_plugin::DefaultRobotSim,
+                        gazebo_ros_control_sim_interface::RobotSim)
+
+#endif // #ifndef __GAZEBO_ROS_CONTROL_PLUGIN_DEFAULT_ROBOT_SIM_H_
