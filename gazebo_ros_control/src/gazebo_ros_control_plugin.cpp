aa865dc91b11d099bb93aa5ad4df79afc47919bd
Dave Coleman <davetcoleman@gmail.com>
2013-08-21 23:27:15 -0700

    Created a header file for the ros_control gazebo plugin

--- gazebo_ros_control/src/gazebo_ros_control_plugin.cpp
+++ gazebo_ros_control/src/gazebo_ros_control_plugin.cpp
@@ -42,288 +42,230 @@
 // Boost
 #include <boost/bind.hpp>
 
-// ROS
-#include <ros/ros.h>
-#include <pluginlib/class_loader.h>
-
-// Gazebo
-#include <gazebo/gazebo.hh>
-#include <gazebo/physics/physics.hh>
-#include <gazebo/common/common.hh>
-#include <gazebo/common/common.hh>
-
-// ros_control
-#include <controller_manager/controller_manager.h>
-#include <gazebo_ros_control/robot_hw_sim.h>
-#include <transmission_interface/transmission_parser.h>
+#include <gazebo_ros_control/gazebo_ros_control_plugin.h>
 
 namespace gazebo_ros_control
 {
 
-class RosControlPlugin : public gazebo::ModelPlugin
+RosControlPlugin::~RosControlPlugin()
 {
-public:
+  // Disconnect from gazebo events
+  gazebo::event::Events::DisconnectWorldUpdateBegin(update_connection_);
+}
 
-  virtual ~RosControlPlugin()
+// Overloaded Gazebo entry point
+void RosControlPlugin::Load(gazebo::physics::ModelPtr parent, sdf::ElementPtr sdf)
+{
+  ROS_INFO_STREAM_NAMED("gazebo_ros_control","Loading gazebo_ros_control plugin...");
+
+  // Save pointers to the model
+  parent_model_ = parent;
+  sdf_ = sdf;
+
+  // ros callback queue for processing subscription
+  deferred_load_thread_ = boost::thread(
+    boost::bind(&RosControlPlugin::loadThread, this));
+}
+
+// Load in seperate thread from Gazebo in case ROS is blocking
+void RosControlPlugin::loadThread()
+{
+  // Error message if the model couldn't be found
+  if (!parent_model_)
   {
-    // Disconnect from gazebo events
-    gazebo::event::Events::DisconnectWorldUpdateBegin(update_connection_);
+    ROS_ERROR_STREAM_NAMED("loadThread","parent model is NULL");
+    return;
   }
 
-  // Overloaded Gazebo entry point
-  void Load(gazebo::physics::ModelPtr parent, sdf::ElementPtr sdf)
+  // Check that ROS has been initialized
+  if(!ros::isInitialized())
   {
-    ROS_INFO_STREAM("Loading gazebo_ros_control plugin...");
-
-    // Save pointers to the model
-    parent_model_ = parent;
-    sdf_ = sdf;
-
-    // ros callback queue for processing subscription
-    deferred_load_thread_ = boost::thread(
-      boost::bind(&RosControlPlugin::loadThread, this));
+    ROS_FATAL_STREAM_NAMED("gazebo_ros_control","A ROS node for Gazebo has not been initialized, unable to load plugin. "
+      << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
+    return;
   }
 
-  // Load in seperate thread from Gazebo in case ROS is blocking
-  void loadThread()
+  // Debug output
+  /*
+    ROS_DEBUG_STREAM_NAMED("ros_control","Plugin XML:");
+    std::string temp;
+    sdf_->PrintValues(temp);
+    std::cout << temp;
+  */
+
+  // Get namespace for nodehandle
+  if(sdf_->HasElement("robotNamespace"))
   {
-    // Error message if the model couldn't be found
-    if (!parent_model_)
+    robot_namespace_ = sdf_->GetElement("robotNamespace")->Get<std::string>();
+  }
+  else
+  {
+    robot_namespace_ = parent_model_->GetName(); // default
+  }
+
+  // Get robot_description ROS param name
+  if (sdf_->HasElement("robotParam"))
+  {
+    robot_description_ = sdf_->GetElement("robotParam")->Get<std::string>();
+  }
+  else
+  {
+    robot_description_ = "robot_description"; // default
+  }
+
+  // Get the robot simulation interface type
+  if(sdf_->HasElement("robotSimType"))
+  {
+    robot_hw_sim_type_str_ = sdf_->Get<std::string>("robotSimType");
+  }
+  else
+  {
+    robot_hw_sim_type_str_ = "gazebo_ros_control/DefaultRobotHWSim";
+    ROS_DEBUG_STREAM_NAMED("loadThread","Using default plugin for RobotHWSim (none specified in URDF/SDF)\""<<robot_hw_sim_type_str_<<"\"");
+  }
+
+  // Get the Gazebo simulation period
+  ros::Duration gazebo_period(parent_model_->GetWorld()->GetPhysicsEngine()->GetUpdatePeriod());
+
+  // Decide the plugin control period
+  if(sdf_->HasElement("controlPeriod"))
+  {
+    control_period_ = ros::Duration(sdf_->Get<double>("controlPeriod"));
+
+    // Check the period against the simulation period
+    if( control_period_ < gazebo_period )
     {
-      ROS_ERROR_STREAM_NAMED("loadThread","parent model is NULL");
+      ROS_ERROR_STREAM_NAMED("gazebo_ros_control","Desired controller update period ("<<control_period_
+        <<" s) is faster than the gazebo simulation period ("<<gazebo_period<<" s).");
+    }
+    else if( control_period_ > gazebo_period )
+    {
+      ROS_WARN_STREAM_NAMED("gazebo_ros_control","Desired controller update period ("<<control_period_
+        <<" s) is slower than the gazebo simulation period ("<<gazebo_period<<" s).");
+    }
+  }
+  else
+  {
+    control_period_ = gazebo_period;
+    ROS_DEBUG_STREAM_NAMED("gazebo_ros_control","Control period not found in URDF/SDF, defaulting to Gazebo period of "
+      << control_period_);
+  }
+
+
+  // Get parameters/settings for controllers from ROS param server
+  model_nh_ = ros::NodeHandle(robot_namespace_);
+  nh_ = ros::NodeHandle();
+  ROS_INFO_NAMED("gazebo_ros_control", "Starting gazebo_ros_control plugin in namespace: %s", robot_namespace_.c_str());
+
+  // Read urdf from ros parameter server then
+  // setup actuators and mechanism control node.
+  // This call will block if ROS is not properly initialized.
+  if (!parseTransmissionsFromURDF())
+  {
+    ROS_ERROR("gazebo_ros_control", "Error parsing URDF in gazebo_ros_control plugin, plugin not active.\n");
+    return;
+  }
+
+  // Load the RobotHWSim abstraction to interface the controllers with the gazebo model
+  try
+  {
+    robot_hw_sim_loader_.reset
+      (new pluginlib::ClassLoader<gazebo_ros_control::RobotHWSim>
+        ("gazebo_ros_control",
+          "gazebo_ros_control::RobotHWSim"));
+
+    robot_hw_sim_ = robot_hw_sim_loader_->createInstance(robot_hw_sim_type_str_);
+
+    if(!robot_hw_sim_->initSim(model_nh_, parent_model_, transmissions_))
+    {
+      ROS_FATAL_NAMED("gazebo_ros_control","Could not initialize robot simulation interface");
       return;
     }
 
-    // Check that ROS has been initialized
-    if(!ros::isInitialized())
+    // Create the controller manager
+    ROS_DEBUG_STREAM_NAMED("ros_control_plugin","Loading controller_manager");
+    controller_manager_.reset
+      (new controller_manager::ControllerManager(robot_hw_sim_.get(), model_nh_));
+
+    // Listen to the update event. This event is broadcast every simulation iteration.
+    update_connection_ =
+      gazebo::event::Events::ConnectWorldUpdateBegin
+      (boost::bind(&RosControlPlugin::Update, this));
+
+  }
+  catch(pluginlib::LibraryLoadException &ex)
+  {
+    ROS_FATAL_STREAM_NAMED("gazebo_ros_control","Failed to create robot simulation interface loader: "<<ex.what());
+  }
+
+  ROS_INFO_NAMED("gazebo_ros_control", "Loaded gazebo_ros_control.");
+}
+
+// Called by the world update start event
+void RosControlPlugin::Update()
+{
+  // Get the simulation time and period
+  gazebo::common::Time gz_time_now = parent_model_->GetWorld()->GetSimTime();
+  ros::Time sim_time_ros(gz_time_now.sec, gz_time_now.nsec);
+  ros::Duration sim_period = sim_time_ros - last_sim_time_ros_;
+
+  // Check if we should update the controllers
+  if(sim_period >= control_period_) {
+    // Store this simulation time
+    last_sim_time_ros_ = sim_time_ros;
+
+    // Update the robot simulation with the state of the gazebo model
+    robot_hw_sim_->readSim(sim_time_ros, sim_period);
+
+    // Compute the controller commands
+    controller_manager_->update(sim_time_ros, sim_period);
+
+    // Update the gazebo model with the result of the controller
+    // computation
+    robot_hw_sim_->writeSim(sim_time_ros, sim_period);
+  }
+}
+
+// Get the URDF XML from the parameter server
+std::string RosControlPlugin::getURDF(std::string param_name) const
+{
+  std::string urdf_string;
+
+  // search and wait for robot_description on param server
+  while (urdf_string.empty())
+  {
+    std::string search_param_name;
+    if (nh_.searchParam(param_name, search_param_name))
     {
-      ROS_ERROR_STREAM("Not loading plugin since ROS hasn't been "
-        << "properly initialized.  Try starting gazebo with ros plugin:\n"
-        << "  gazebo -s libgazebo_ros_api_plugin.so\n");
+      ROS_INFO_ONCE_NAMED("gazebo_ros_control", "gazebo_ros_control plugin is waiting for model"
+        " URDF in parameter [%s] on the ROS param server.", search_param_name.c_str());
 
-      return;
-    }
-
-    // Debug output
-    /*
-      ROS_DEBUG_STREAM_NAMED("ros_control","Plugin XML:");
-      std::string temp;
-      sdf_->PrintValues(temp);
-      std::cout << temp;
-    */
-
-    // Get namespace for nodehandle
-    // TODO: "topicNamespace" or "rosNamespace"
-    if(sdf_->HasElement("robotNamespace")) {
-      robot_namespace_ = sdf_->GetElement("robotNamespace")->Get<std::string>();
-    } else {
-      // TODO: Why not make this default?
-      robot_namespace_ = parent_model_->GetName(); // default
-    }
-
-    // Get robot_description ROS param name
-    if (sdf_->HasElement("robotParam")) {
-      robot_description_ = sdf_->GetElement("robotParam")->Get<std::string>();
-    } else {
-      robot_description_ = "robot_description"; // default
-    }
-
-    // Prefix robot description with namespace
-    //robot_description_ = robot_namespace_ + "/" + robot_description_;
-    //ROS_DEBUG_STREAM_NAMED("loadThread","Using rosparam parameter \""<<robot_description_<<"\" for robot model description.");
-
-    // Get the robot simulation interface type
-    if(sdf_->HasElement("robotSimType")) {
-      robot_hw_sim_type_str_ = sdf_->Get<std::string>("robotSimType");
-    } else {
-      robot_hw_sim_type_str_ = "gazebo_ros_control/DefaultRobotHWSim";
-      ROS_DEBUG_STREAM_NAMED("loadThread","Using default plugin for RobotHWSim (none specified in URDF/SDF)\""<<robot_hw_sim_type_str_<<"\"");
-    }
-
-    // Get the Gazebo simulation period
-    ros::Duration sim_period(parent_model_->GetWorld()->GetPhysicsEngine()->GetUpdatePeriod());
-
-    // Decide the plugin control period
-    if(sdf_->HasElement("controlPeriod"))
-    {
-      control_period_ = ros::Duration(sdf_->Get<double>("controlPeriod"));
-
-      // Check the period against the simulation period
-      if( control_period_ < sim_period )
-      {
-        ROS_ERROR_STREAM("Desired controller update period ("<<control_period_
-          <<" s) is faster than the gazebo simulation period ("<<sim_period<<" s).");
-      }
-      else if( control_period_ > sim_period )
-      {
-        ROS_WARN_STREAM("Desired controller update period ("<<control_period_
-          <<" s) is slower than the gazebo simulation period ("<<sim_period<<" s).");
-      }
+      nh_.getParam(search_param_name, urdf_string);
     }
     else
     {
-      control_period_ = sim_period;
-      ROS_DEBUG_STREAM("Control period not found in URDF/SDF, defaulting to Gazebo period of "
-        << control_period_);
+      ROS_INFO_ONCE_NAMED("gazebo_ros_control", "gazebo_ros_control plugin is waiting for model"
+        " URDF in parameter [%s] on the ROS param server.", robot_description_.c_str());
+
+      nh_.getParam(param_name, urdf_string);
     }
 
-
-    // Get parameters/settings for controllers from ROS param server
-    model_nh_ = ros::NodeHandle(robot_namespace_);
-    nh_ = ros::NodeHandle();
-    ROS_INFO("Starting gazebo_ros_control plugin in namespace: %s", robot_namespace_.c_str());
-
-    // Read urdf from ros parameter server then
-    // setup actuators and mechanism control node.
-    // This call will block if ROS is not properly initialized.
-    if (!parseTransmissionsFromURDF())
-    {
-      ROS_ERROR("Error parsing URDF in gazebo_ros_control plugin, plugin not active.\n");
-      return;
-    }
-
-    // Load the RobotHWSim abstraction to interface the controllers with the
-    // gazebo model
-    try
-    {
-      robot_hw_sim_loader_.reset
-        (new pluginlib::ClassLoader<gazebo_ros_control::RobotHWSim>
-          ("gazebo_ros_control",
-            "gazebo_ros_control::RobotHWSim"));
-
-      robot_hw_sim_ = robot_hw_sim_loader_->createInstance(robot_hw_sim_type_str_);
-
-      if(!robot_hw_sim_->initSim(model_nh_, parent_model_, transmissions_))
-      {
-        ROS_FATAL("Could not initialize robot simulation interface");
-        return;
-      }
-
-      // Create the controller manager
-      ROS_DEBUG_STREAM_NAMED("ros_control_plugin","Loading controller_manager");
-      controller_manager_.reset
-        (new controller_manager::ControllerManager(robot_hw_sim_.get(), model_nh_));
-
-      // Listen to the update event. This event is broadcast every simulation iteration.
-      update_connection_ =
-        gazebo::event::Events::ConnectWorldUpdateBegin
-        (boost::bind(&RosControlPlugin::Update, this));
-
-    }
-    catch(pluginlib::LibraryLoadException &ex)
-    {
-      ROS_FATAL_STREAM("Failed to create robot simulation interface loader: "<<ex.what());
-    }
-
-    ROS_INFO("Loaded gazebo_ros_control.");
+    usleep(100000);
   }
+  ROS_DEBUG_STREAM_NAMED("gazebo_ros_control", "Recieved urdf from param server, parsing...");
 
-  // Called by the world update start event
-  void Update()
-  {
-    // Get the simulation time and period
-    gazebo::common::Time gz_time_now = parent_model_->GetWorld()->GetSimTime();
-    ros::Time sim_time_ros(gz_time_now.sec, gz_time_now.nsec);
-    ros::Duration sim_period = sim_time_ros - last_sim_time_ros_;
+  return urdf_string;
+}
 
-    // Check if we should update the controllers
-    if(sim_period >= control_period_) {
-      // Store this simulation time
-      last_sim_time_ros_ = sim_time_ros;
+// Get Transmissions from the URDF
+bool RosControlPlugin::parseTransmissionsFromURDF()
+{
+  std::string urdf_string = getURDF(robot_description_);
 
-      // Update the robot simulation with the state of the gazebo model
-      robot_hw_sim_->readSim(sim_time_ros, sim_period);
+  transmission_interface::TransmissionParser::parse(urdf_string, transmissions_);
 
-      // Compute the controller commands
-      controller_manager_->update(sim_time_ros, sim_period);
+  return true;
+}
 
-      // Update the gazebo model with the result of the controller
-      // computation
-      robot_hw_sim_->writeSim(sim_time_ros, sim_period);
-    }
-  }
 
-  // Get the URDF XML from the parameter server
-  std::string getURDF(std::string param_name) const
-  {
-    std::string urdf_string;
-
-    // search and wait for robot_description on param server
-    while (urdf_string.empty())
-    {
-      std::string search_param_name;
-      if (nh_.searchParam(param_name, search_param_name))
-      {
-        ROS_INFO_ONCE("gazebo_ros_control plugin is waiting for model"
-          " URDF in parameter [%s] on the ROS param server.", search_param_name.c_str());
-
-        nh_.getParam(search_param_name, urdf_string);
-      }
-      else
-      {
-        ROS_INFO_ONCE("gazebo_ros_control plugin is waiting for model"
-          " URDF in parameter [%s] on the ROS param server.", robot_description_.c_str());
-
-        nh_.getParam(param_name, urdf_string);
-      }
-
-      usleep(100000);
-    }
-    ROS_DEBUG_STREAM_NAMED("gazebo_ros_control", "Recieved urdf from param server, parsing...");
-
-    return urdf_string;
-  }
-
-  // Get Transmissions from the URDF
-  bool parseTransmissionsFromURDF()
-  {
-    std::string urdf_string = getURDF(robot_description_);
-
-    transmission_interface::TransmissionParser::parse(urdf_string, transmissions_);
-
-    return true;
-  }
-
-private:
-
-  // Node Handles
-  ros::NodeHandle nh_; // no namespace
-  ros::NodeHandle model_nh_; // namespaces to robot name
-
-  // Pointer to the model
-  gazebo::physics::ModelPtr parent_model_;
-  sdf::ElementPtr sdf_;
-
-  // deferred load in case ros is blocking
-  boost::thread deferred_load_thread_;
-
-  // Pointer to the update event connection
-  gazebo::event::ConnectionPtr update_connection_;
-
-  // Interface loader
-  boost::shared_ptr<pluginlib::ClassLoader<gazebo_ros_control::RobotHWSim> > robot_hw_sim_loader_;
-  void load_robot_hw_sim_srv();
-
-  // Strings
-  std::string robot_namespace_;
-  std::string robot_description_;
-
-  // Transmissions in this plugin's scope
-  std::vector<transmission_interface::TransmissionInfo> transmissions_;
-
-  // Robot simulator interface
-  std::string robot_hw_sim_type_str_;
-  boost::shared_ptr<gazebo_ros_control::RobotHWSim> robot_hw_sim_;
-
-  // Controller manager
-  boost::shared_ptr<controller_manager::ControllerManager> controller_manager_;
-
-  // Timing
-  ros::Duration control_period_;
-  ros::Time last_sim_time_ros_;
-
-};
-
-// Register this plugin with the simulator
-GZ_REGISTER_MODEL_PLUGIN(RosControlPlugin);
 }
