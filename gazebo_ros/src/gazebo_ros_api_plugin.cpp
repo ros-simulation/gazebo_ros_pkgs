639c516162f631974db7340fd657c81f8d02dcfe
Markus Bader <markus.bader@tuwien.ac.at>
2014-11-21 12:16:07 +0100

    foramt fixed

--- gazebo_ros/src/gazebo_ros_api_plugin.cpp
+++ gazebo_ros/src/gazebo_ros_api_plugin.cpp
@@ -780,10 +780,10 @@ bool GazeboRosApiPlugin::getModelState(gazebo_msgs::GetModelState::Request &req,
     std::map<std::string, unsigned int>::iterator it = access_count_get_model_state_.find(req.model_name);
     if(it == access_count_get_model_state_.end())
     {
-      access_count_get_model_state_.insert(std::pair<std::string, unsigned int>(req.model_name, 1) );
+      access_count_get_model_state_.insert(std::pair<std::string, unsigned int>(req.model_name, 1));
       res.header.seq = 1;
-    } 
-    else 
+    }
+    else
     {
       it->second++;
       res.header.seq = it->second;
