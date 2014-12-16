67abdd9b74d4e1ddd8f744905f7ca3879c5b7799
Jim Rothrock <jim.rothrock@wunderkammerlab.com>
2014-12-16 10:50:46 -0800

    Added emergency stop support.

--- gazebo_ros_control/include/gazebo_ros_control/robot_hw_sim.h
+++ gazebo_ros_control/include/gazebo_ros_control/robot_hw_sim.h
@@ -34,10 +34,10 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  *********************************************************************/
 
-/* 
-   Author: Jonathon Brohen, Dave Coleman
-   Desc:   Plugin template for hardware interfaces for ros_control and Gazebo
-*/
+/// \brief Plugin template for hardware interfaces for ros_control and Gazebo
+
+/// \author Jonathan Bohren
+/// \author Dave Coleman
 
 #ifndef __ROS_CONTROL_GAZEBO_ROBOT_HW_SIM_H
 #define __ROS_CONTROL_GAZEBO_ROBOT_HW_SIM_H
@@ -62,13 +62,26 @@ namespace gazebo_ros_control {
     {}
   };
 
-  // Gazebo plugin version of RobotHW
+  /// \brief Gazebo plugin version of RobotHW
+  ///
+  /// An object of class RobotHWSim represents a robot's simulated hardware.
   class RobotHWSim : public hardware_interface::RobotHW 
   {
   public:
 
     virtual ~RobotHWSim() { }
 
+    /// \brief Initialize the simulated robot hardware
+    ///
+    /// Initialize the simulated robot hardware.
+    ///
+    /// \param robot_namespace  Robot namespace.
+    /// \param model_nh  Model node handle.
+    /// \param parent_model  Parent model.
+    /// \param urdf_model  URDF model.
+    /// \param transmissions  Transmissions.
+    ///
+    /// \return  \c true if the simulated robot hardware is initialized successfully, \c false if not.
     virtual bool initSim(
         const std::string& robot_namespace,
         ros::NodeHandle model_nh, 
@@ -76,9 +89,28 @@ namespace gazebo_ros_control {
         const urdf::Model *const urdf_model,
         std::vector<transmission_interface::TransmissionInfo> transmissions) = 0;
 
+    /// \brief Read state data from the simulated robot hardware
+    ///
+    /// Read state data, such as joint positions and velocities, from the simulated robot hardware.
+    ///
+    /// \param time  Simulation time.
+    /// \param period  Time since the last simulation step.
     virtual void readSim(ros::Time time, ros::Duration period) = 0;
 
+    /// \brief Write commands to the simulated robot hardware
+    ///
+    /// Write commands, such as joint position and velocity commands, to the simulated robot hardware.
+    ///
+    /// \param time  Simulation time.
+    /// \param period  Time since the last simulation step.
     virtual void writeSim(ros::Time time, ros::Duration period) = 0;
+
+    /// \brief Set the emergency stop state
+    ///
+    /// Set the simulated robot's emergency stop state. The default implementation of this function does nothing.
+    ///
+    /// \param active  \c true if the emergency stop is active, \c false if not.
+    virtual void eStopActive(const bool active) {}
 
   };
 
