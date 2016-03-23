5fa7cc85724a851c639d33b9172ecf4dfc2029af
Lucas Walter <wsacul@gmail.com>
2016-03-23 05:52:53 -0700

    #408 Make the multi camera timestamps current rather than outdated, also reuse the same update code

--- gazebo_plugins/include/gazebo_plugins/gazebo_ros_multicamera.h
+++ gazebo_plugins/include/gazebo_plugins/gazebo_ros_multicamera.h
@@ -42,6 +42,8 @@ namespace gazebo
 
     std::vector<GazeboRosCameraUtils*> utils;
 
+    protected: void OnNewFrame(const unsigned char *_image,
+                   GazeboRosCameraUtils* util);
     /// \brief Update the controller
     /// FIXME: switch to function vectors
     protected: virtual void OnNewFrameLeft(const unsigned char *_image,
