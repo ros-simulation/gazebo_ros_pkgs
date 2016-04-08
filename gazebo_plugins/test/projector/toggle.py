7af71ea3d119197e9e114067a5cd7b403c28804a
Lucas Walter <wsacul@gmail.com>
2016-04-08 04:23:30 -0700

    Still no crashes with two alternating projectors

--- gazebo_plugins/test/projector/toggle.py
+++ gazebo_plugins/test/projector/toggle.py
@@ -7,11 +7,13 @@ from std_msgs.msg import Int32
 rospy.init_node('toggle')
 
 pub = rospy.Publisher("/projector_plugin/projector", Int32, queue_size=5)
+pub2 = rospy.Publisher("/projector_plugin2/projector", Int32, queue_size=5)
 toggle = True
 start_time = None
 while not rospy.is_shutdown():
     for i in range(3):
         pub.publish(Int32(int(toggle)))
+        pub2.publish(Int32(int(not toggle)))
         toggle = not toggle
     rospy.sleep(0.5)
     cur_time = rospy.Time.now().to_sec()
