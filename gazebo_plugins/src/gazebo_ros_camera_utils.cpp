b8cbe5a26aa8cb8d1d1e5ad0e2d50c44f9f43da4
Dave Coleman <davetcoleman@gmail.com>
2017-02-16 11:20:50 +0530

    Remove compiler directive flags for < GAZEBO 7

--- gazebo_plugins/src/gazebo_ros_camera_utils.cpp
+++ gazebo_plugins/src/gazebo_ros_camera_utils.cpp
@@ -352,11 +352,7 @@ void GazeboRosCameraUtils::LoadThread()
 // Set Horizontal Field of View
 void GazeboRosCameraUtils::SetHFOV(const std_msgs::Float64::ConstPtr& hfov)
 {
-#if GAZEBO_MAJOR_VERSION >= 7
   this->camera_->SetHFOV(ignition::math::Angle(hfov->data));
-#else
-  this->camera_->SetHFOV(gazebo::math::Angle(hfov->data));
-#endif
 }
 
 ////////////////////////////////////////////////////////////////////////////////
