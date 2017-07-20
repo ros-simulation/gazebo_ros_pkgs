a7b2d3859e1183a59db101d0b920a26034e2b0b9
Jordan Liviero <jliviero@clearpathrobotics.com>
2017-07-20 12:47:09 -0400

    Compute twist in BODY frame for p3d plugin

--- gazebo_plugins/src/gazebo_ros_p3d.cpp
+++ gazebo_plugins/src/gazebo_ros_p3d.cpp
@@ -287,6 +287,12 @@ void GazeboRosP3D::UpdateChild()
           vpos = frame_pose.Rot().RotateVector(vpos - frame_vpos);
           veul = frame_pose.Rot().RotateVector(veul - frame_veul);
         }
+        else
+        {
+          const math::Quaternion rot_inv = pose.rot.GetInverse();
+          vpos = rot_inv.RotateVector(vpos);
+          veul = rot_inv.RotateVector(veul);
+        }
 
         // Apply Constant Offsets
         // apply xyz offsets and get position and rotation components
