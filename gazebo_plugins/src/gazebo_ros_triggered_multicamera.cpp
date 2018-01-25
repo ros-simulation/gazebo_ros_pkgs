a2e936ba70da9f4c08db51c31f5de6b7fbdb2115
Steven Peters <scpeters@openrobotics.org>
2018-01-25 15:38:03 -0800

    update copyright dates and remove copied comments

--- gazebo_plugins/src/gazebo_ros_triggered_multicamera.cpp
+++ gazebo_plugins/src/gazebo_ros_triggered_multicamera.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Open Source Robotics Foundation
+ * Copyright 2017 Open Source Robotics Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,11 +14,6 @@
  * limitations under the License.
  *
 */
-/*
- * Desc: Syncronizes shutters across multiple cameras
- * Author: John Hsu
- * Date: 10 June 2013
- */
 
 #include <string>
 
