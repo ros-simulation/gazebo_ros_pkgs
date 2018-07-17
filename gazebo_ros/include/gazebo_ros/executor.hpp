5352c3ac26491e39c89d71972d32a1bf84d94ff2
Kevin Allen <kallen@osrfoundation.org>
2018-07-17 14:12:09 -0700

    Create base Node class for gazebo plugins with ROS2

---
+++ gazebo_ros/include/gazebo_ros/executor.hpp
@@ -0,0 +1,52 @@
+// Copyright 2018 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GAZEBO_ROS_EXECUTOR_HPP
+#define GAZEBO_ROS_EXECUTOR_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <gazebo/common/common.hh>
+
+namespace gazebo_ros
+{
+
+/// Executor run in a separate thread to handle events from all #gazebo_ros::Node instances
+/**
+ * \class Executor executor.hpp <gazebo_ros/executor.hpp>
+ */
+class Executor : public rclcpp::executors::MultiThreadedExecutor
+{
+public:
+  /// Create an instance and start the internal thread
+  Executor();
+
+  /// Stops the internal executor and joins with the internal thread
+  virtual ~Executor();
+
+private:
+  /// Thread where the executor spins until destruction
+  std::thread spin_thread_;
+
+  /// Spin executor, called in #spin_thread_
+  void run();
+
+  /// Shutdown ROS, called when gazebo sigint handle arrives so ROS is shutdown cleanly
+  void shutdown();
+
+  /// Connection to gazebo sigint event
+  gazebo::event::ConnectionPtr sigint_handle_;
+};
+
+} // namespace gazebo_ros
+#endif
---
+++ gazebo_ros/src/node.cpp
@@ -0,0 +1,51 @@
+// Copyright 2018 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gazebo_ros/node.hpp>
+
+namespace gazebo_ros
+{
+
+std::weak_ptr<Executor> Node::static_executor_;
+std::mutex Node::lock_;
+std::atomic_bool Node::initialized_(false);
+
+Node::NotInitializedException::NotInitializedException() :
+  std::runtime_error("Called LoadNode before InitROS. Be sure to load the ros_api plugin first")
+{
+}
+
+Node::~Node()
+{
+}
+
+void Node::InitROS(int argc, char** argv)
+{
+  std::lock_guard<std::mutex> l(lock_); 
+  if (initialized_)
+  {
+    RCLCPP_WARN(rclcpp::get_logger("gazebo_ros_node"), "Called Node::InitROS twice, ignoring second call.");
+    return;
+  }
+
+  rclcpp::init(argc, argv);
+  initialized_ = true;
+}
+
+Node::SharedPtr Node::Create(const std::string& node_name)
+{
+  return Create<const std::string&>(node_name);
+}
+
+} // namespace gazebo_ros
