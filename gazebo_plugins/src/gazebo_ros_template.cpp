c47a08706ab2ee119e50476accfcd2f1779bab73
Louise Poubel <louise@osrfoundation.org>
2018-07-27 13:08:07 -0700

    improve example, add demo world, fix sdf warnings

--- gazebo_plugins/src/gazebo_ros_template.cpp
+++ gazebo_plugins/src/gazebo_ros_template.cpp
@@ -12,28 +12,28 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <rclcpp/rclcpp.hpp>
-#include <gazebo_ros/node.hpp>
-
 #include <gazebo/physics/Model.hh>
-
 #include <gazebo_plugins/gazebo_ros_template.hpp>
+#include <gazebo_ros/node.hpp>
+#include <rclcpp/rclcpp.hpp>
 
-namespace gazebo
+#include <memory>
+
+namespace gazebo_plugins
 {
 /// Class to hold private data members (PIMPL pattern)
 class GazeboRosTemplatePrivate
 {
 public:
   /// Connection to world update event. Callback is called while this is alive.
-  event::ConnectionPtr update_connection_;
+  gazebo::event::ConnectionPtr update_connection_;
 
   /// Node for ROS communication.
   gazebo_ros::Node::SharedPtr ros_node_;
 };
 
 GazeboRosTemplate::GazeboRosTemplate()
-: d_ptr_(new GazeboRosTemplatePrivate)
+: impl_(std::make_unique<GazeboRosTemplatePrivate>())
 {
 }
 
@@ -41,20 +41,20 @@ GazeboRosTemplate::~GazeboRosTemplate()
 {
 }
 
-void GazeboRosTemplate::Load(physics::ModelPtr model, sdf::ElementPtr sdf)
+void GazeboRosTemplate::Load(gazebo::physics::ModelPtr model, sdf::ElementPtr sdf)
 {
   // Create a GazeboRos node instead of a common ROS node.
   // Pass it SDF parameters so common options like namespace and remapping
   // can be handled.
-  d_ptr_->ros_node_ = gazebo_ros::Node::Create("gazebo_ros_template", sdf);
+  impl_->ros_node_ = gazebo_ros::Node::Create("gazebo_ros_template", sdf);
 
   // The model pointer gives you direct access to the physics object,
   // for example:
-  RCLCPP_INFO(d_ptr_->ros_node_->get_logger(), model->GetName().c_str());
+  RCLCPP_INFO(impl_->ros_node_->get_logger(), model->GetName().c_str());
 
   // Create a connection so the OnUpdate function is called at every simulation
   // iteration. Remove this call, the connection and the callback if not needed.
-  d_ptr_->update_connection_ = event::Events::ConnectWorldUpdateBegin(
+  impl_->update_connection_ = gazebo::event::Events::ConnectWorldUpdateBegin(
     std::bind(&GazeboRosTemplate::OnUpdate, this));
 }
 
@@ -65,4 +65,4 @@ void GazeboRosTemplate::OnUpdate()
 
 // Register this plugin with the simulator
 GZ_REGISTER_MODEL_PLUGIN(GazeboRosTemplate)
-}  // namespace gazebo
+}  // namespace gazebo_plugins
