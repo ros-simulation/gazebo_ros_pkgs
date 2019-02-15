da22e394971278f3392df6f46686ad93ac6660c6
chapulina <louise@openrobotics.org>
2019-02-15 09:38:45 -0800

    [ros2] Port time commands (pause / reset) (#866)

--- .ros1_unported/gazebo_ros/include/gazebo_ros/gazebo_ros_api_plugin.h
+++ .ros1_unported/gazebo_ros/include/gazebo_ros/gazebo_ros_api_plugin.h
@@ -166,18 +166,6 @@ public:
   bool applyJointEffort(gazebo_msgs::ApplyJointEffort::Request &req,gazebo_msgs::ApplyJointEffort::Response &res);
 
   /// \brief
-  bool resetSimulation(std_srvs::Empty::Request &req,std_srvs::Empty::Response &res);
-
-  /// \brief
-  bool resetWorld(std_srvs::Empty::Request &req,std_srvs::Empty::Response &res);
-
-  /// \brief
-  bool pausePhysics(std_srvs::Empty::Request &req,std_srvs::Empty::Response &res);
-
-  /// \brief
-  bool unpausePhysics(std_srvs::Empty::Request &req,std_srvs::Empty::Response &res);
-
-  /// \brief
   bool clearJointForces(gazebo_msgs::JointRequest::Request &req,gazebo_msgs::JointRequest::Response &res);
   bool clearJointForces(std::string joint_name);
 
@@ -263,10 +251,6 @@ private:
   ros::ServiceServer set_joint_properties_service_;
   ros::ServiceServer apply_joint_effort_service_;
   ros::ServiceServer set_model_configuration_service_;
-  ros::ServiceServer reset_simulation_service_;
-  ros::ServiceServer reset_world_service_;
-  ros::ServiceServer pause_physics_service_;
-  ros::ServiceServer unpause_physics_service_;
   ros::ServiceServer clear_joint_forces_service_;
   ros::ServiceServer clear_body_wrenches_service_;
   ros::Publisher     pub_link_states_;
