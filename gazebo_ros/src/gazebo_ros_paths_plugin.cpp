9514539a7f0313b126b80b33426f4f7653890ab7
Sean Yen <seanyen@microsoft.com>
2019-06-26 06:44:42 -0700

    provide Windows implemenation for setenv. (#879)

--- gazebo_ros/src/gazebo_ros_paths_plugin.cpp
+++ gazebo_ros/src/gazebo_ros_paths_plugin.cpp
@@ -27,6 +27,19 @@
 
 #include <map>
 
+#ifdef _WIN32
+int setenv(const char *name, const char *value, int overwrite)
+{
+    int errcode = 0;
+    if(!overwrite) {
+        size_t envsize = 0;
+        errcode = ::getenv_s(&envsize, NULL, 0, name);
+        if(errcode || envsize) return errcode;
+    }
+    return ::_putenv_s(name, value);
+}
+#endif
+
 namespace gazebo
 {
 
