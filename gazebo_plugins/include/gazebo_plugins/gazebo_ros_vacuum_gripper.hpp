8c544be48e07ff98ae1ce8f15379993f2114d890
Shivesh Khaitan <shiveshkhaitan@gmail.com>
2019-08-14 21:20:52 +0530

    [ros2] Port vacuum gripper to ROS2 (#960)
    * [ros2] Port vacuum gripper to ROS2
    
    * Fix gripper forces
    
    * Add option to set max_distance
    
    Change SetForce -> Add Force

---
+++ gazebo_plugins/include/gazebo_plugins/gazebo_ros_vacuum_gripper.hpp
@@ -0,0 +1,79 @@
+// Copyright 2019 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GAZEBO_PLUGINS__GAZEBO_ROS_VACUUM_GRIPPER_HPP_
+#define GAZEBO_PLUGINS__GAZEBO_ROS_VACUUM_GRIPPER_HPP_
+
+#include <gazebo/common/Plugin.hh>
+
+#include <memory>
+
+namespace gazebo_plugins
+{
+class GazeboRosVacuumGripperPrivate;
+
+/// Vacuum Gripper plugin for attracting entities around the model like vacuum
+/*
+ * \author  Kentaro Wada
+ *
+ * \date 7 Dec 2015
+ */
+/**
+  Example Usage:
+  \code{.xml}
+    <plugin name='vacuum_gripper' filename='libgazebo_ros_vacuum_gripper.so'>
+
+      <ros>
+
+        <!-- Add a namespace -->
+        <namespace>/demo</namespace>
+
+        <!-- Remapping service and topic names -->
+        <argument>switch:=custom_switch</argument>
+        <argument>grasping:=custom_grasping</argument>
+      </ros>
+
+      <!-- Link associated with gripper -->
+      <link_name>link</link_name>
+
+      <!-- Max distance to attract entities -->
+      <max_distance>10.0</max_distance>
+
+      <!-- List of entities to be not attracted by the gripper -->
+      <fixed>ground_plane</fixed>
+      <fixed>wall</fixed>
+
+    </plugin>
+  \endcode
+*/
+class GazeboRosVacuumGripper : public gazebo::ModelPlugin
+{
+public:
+  /// Constructor
+  GazeboRosVacuumGripper();
+
+  /// Destructor
+  ~GazeboRosVacuumGripper();
+
+protected:
+  // Documentation inherited
+  void Load(gazebo::physics::ModelPtr _model, sdf::ElementPtr _sdf) override;
+
+private:
+  /// Private data pointer
+  std::unique_ptr<GazeboRosVacuumGripperPrivate> impl_;
+};
+}  // namespace gazebo_plugins
+
+#endif  // GAZEBO_PLUGINS__GAZEBO_ROS_VACUUM_GRIPPER_HPP_
---
+++ gazebo_plugins/test/test_gazebo_ros_vacuum_gripper.cpp
@@ -0,0 +1,97 @@
+// Copyright 2019 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gazebo/test/ServerFixture.hh>
+#include <rclcpp/rclcpp.hpp>
+#include <std_srvs/srv/set_bool.hpp>
+
+#include <memory>
+
+#define tol 10e-2
+
+class GazeboRosVacuumGripperTest : public gazebo::ServerFixture
+{
+};
+
+TEST_F(GazeboRosVacuumGripperTest, VacuumGripperServiceTest)
+{
+  // Load test world and start paused
+  this->Load("worlds/gazebo_ros_vacuum_gripper.world", true);
+
+  // World
+  auto world = gazebo::physics::get_world();
+  ASSERT_NE(nullptr, world);
+
+  // Box
+  auto box = world->ModelByName("box");
+  ASSERT_NE(nullptr, box);
+
+  // Ball1
+  auto ball1 = world->ModelByName("ball1");
+  ASSERT_NE(nullptr, ball1);
+
+  // Ball2
+  auto ball2 = world->ModelByName("ball2");
+  ASSERT_NE(nullptr, ball2);
+
+  // Check plugin was loaded
+  world->Step(100);
+  EXPECT_EQ(1u, box->GetPluginCount());
+
+  // Check ball1 position
+  EXPECT_NEAR(0.6, ball1->WorldPose().Pos().X(), tol);
+  EXPECT_NEAR(0.0, ball1->WorldPose().Pos().Y(), tol);
+
+  // Check ball2 position
+  EXPECT_NEAR(-0.6, ball2->WorldPose().Pos().X(), tol);
+  EXPECT_NEAR(0.0, ball2->WorldPose().Pos().Y(), tol);
+
+  // Create ROS publisher
+  auto node = std::make_shared<rclcpp::Node>("gazebo_ros_vacuum_gripper_test");
+  ASSERT_NE(nullptr, node);
+
+  auto client = node->create_client<std_srvs::srv::SetBool>("test/switch_test");
+  ASSERT_NE(nullptr, client);
+  EXPECT_TRUE(client->wait_for_service(std::chrono::seconds(1)));
+
+  auto request = std::make_shared<std_srvs::srv::SetBool::Request>();
+  request->data = true;
+  auto response_future = client->async_send_request(request);
+  EXPECT_EQ(rclcpp::executor::FutureReturnCode::SUCCESS,
+    rclcpp::spin_until_future_complete(node, response_future));
+
+  unsigned int sleep = 0;
+  unsigned int max_sleep = 200;
+  while (sleep < max_sleep &&
+    (ball1->WorldPose().Pos().X() > 0.1 || ball2->WorldPose().Pos().Y() < -0.1))
+  {
+    gazebo::common::Time::MSleep(100);
+    world->Step(100);
+    sleep++;
+  }
+
+  // Check balls moved
+  EXPECT_LT(sleep, max_sleep);
+  EXPECT_LT(ball1->WorldPose().Pos().X(), 0.1);
+  EXPECT_NE(ball1->WorldPose().Pos().Y(), 0);
+  EXPECT_GT(ball2->WorldPose().Pos().X(), -0.1);
+  EXPECT_NE(ball2->WorldPose().Pos().Y(), 0);
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
