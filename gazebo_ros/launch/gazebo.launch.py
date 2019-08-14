7b0455ddb7605c298a9b7a186d0121ab65e89cb5
Louise Poubel <louise@openrobotics.org>
2019-08-14 13:29:27 -0700

    [ros2] Conditional launch includes
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- gazebo_ros/launch/gazebo.launch.py
+++ gazebo_ros/launch/gazebo.launch.py
@@ -14,22 +14,32 @@
 
 """Launch Gazebo server and client with command line arguments."""
 
+from launch import Condition
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
 from launch.substitutions import ThisLaunchFileDir
 
 
 def generate_launch_description():
 
-    # (TODO) Allow conditional include of gzserver and gzclient, once supported
-    # https://github.com/ros2/launch/issues/303
     return LaunchDescription([
+        DeclareLaunchArgument('gui', default_value='true',
+                              description='Set to "false" to run headless.'),
+
+        DeclareLaunchArgument('server', default_value='true',
+                              description='Set to "false" not to run gzserver.'),
+
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/gzserver.launch.py']),
+            condition=IfCondition(LaunchConfiguration('server'))
         ),
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([ThisLaunchFileDir(), '/gzclient.launch.py']),
+            condition=IfCondition(LaunchConfiguration('gui'))
         ),
     ])
