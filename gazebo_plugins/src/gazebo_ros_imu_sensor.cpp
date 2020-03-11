1c1ee79cc8f0cedab3abf6880e9973dced6a9ce8
Jacob Perron <jacob@openrobotics.org>
2020-03-11 14:24:16 -0700

    Measure IMU orientation with respect to world
    Report the IMU orientation from the sensor plugin with respect to the world frame.
    This complies with convention documented in REP 145: https://www.ros.org/reps/rep-0145.html
    
    In order to not break existing behavior, users should opt-in by adding a new SDF tag.

--- gazebo_plugins/src/gazebo_ros_imu_sensor.cpp
+++ gazebo_plugins/src/gazebo_ros_imu_sensor.cpp
@@ -66,6 +66,16 @@ void GazeboRosImuSensor::Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementPt
     return;
   }
 
+  if (sdf->HasElement("initial_orientation_as_reference"))
+  {
+    bool initial_orientation_as_reference = sdf->Get<bool>("initial_orientation_as_reference");
+    ROS_INFO_STREAM("<initial_orientation_as_reference> set to: "<< initial_orientation_as_reference);
+    if (!initial_orientation_as_reference) {
+      // This complies with REP 145
+      sensor->SetWorldToReferenceOrientation(ignition::math::Quaterniond::Identity);
+    }
+  }
+
   impl_->pub_ =
     impl_->ros_node_->create_publisher<sensor_msgs::msg::Imu>("~/out", rclcpp::SensorDataQoS());
 
