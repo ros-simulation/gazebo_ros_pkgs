7d0ed4ceeea0c77a606ca59c539aa8d5d7e56dc8
Simon Schmeisser <simon.schmeisser@isys-vision.de>
2020-05-07 22:04:07 +0200

    if pkg-config for gazebo not found, fallback to /usr/share/gazebo

--- gazebo_ros/scripts/libcommon.sh
+++ gazebo_ros/scripts/libcommon.sh
@@ -17,3 +17,22 @@ relocate_remappings()
 
   echo "$gazebo_args$ros_remaps" | cut -c 1-
 }
+
+# In order to avoid a runtime dependency on gazebo_dev in Debian packages
+# a fallback to the default location of /usr/share/gazebo is used.
+# For more information see:
+# https://github.com/ros-simulation/gazebo_ros_pkgs/issues/323
+find_gazebo()
+{
+    if pkg-config --exists gazebo
+    then
+        setup_path=$(pkg-config --variable=prefix gazebo)/share/gazebo
+    elif [ -d "/usr/share/gazebo" ]
+    then
+        echo "using default path /usr/share/gazebo"
+        setup_path=/usr/share/gazebo
+    else
+        echo "gazebo not found, neither via pkg-config nor in /usr/share/gazebo"
+        exit -1
+    fi
+}
