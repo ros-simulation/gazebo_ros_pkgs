8e3f7d7622fa7422d601010ac34cf4aaf06d4c3c
Steve Macenski <stevenmacenski@gmail.com>
2020-10-12 16:49:10 -0700

    adding buffer to gzebo ros hand of god plugin (#1179)
    Fixes #1178.

--- gazebo_plugins/src/gazebo_ros_hand_of_god.cpp
+++ gazebo_plugins/src/gazebo_ros_hand_of_god.cpp
@@ -53,6 +53,7 @@
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
 
 #include <memory>
 #include <string>
