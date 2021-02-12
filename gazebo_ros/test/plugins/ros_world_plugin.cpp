f31628ba641c135956087d631c0ed7079c728c58
Steve Peters <scpeters@openrobotics.org>
2021-02-11 16:13:23 -0800

    fix uncrustify
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- gazebo_ros/test/plugins/ros_world_plugin.cpp
+++ gazebo_ros/test/plugins/ros_world_plugin.cpp
@@ -40,8 +40,9 @@ void RosWorldPlugin::Load(gazebo::physics::WorldPtr, sdf::ElementPtr _sdf)
   assert(nullptr != node);
 
   // Create a publisher
-  auto pub = node->create_publisher<std_msgs::msg::String>("test",
-      rclcpp::QoS(rclcpp::KeepLast(1)).transient_local());
+  auto pub = node->create_publisher<std_msgs::msg::String>(
+    "test",
+    rclcpp::QoS(rclcpp::KeepLast(1)).transient_local());
 
   // Run lambda every 1 second
   using namespace std::chrono_literals;
