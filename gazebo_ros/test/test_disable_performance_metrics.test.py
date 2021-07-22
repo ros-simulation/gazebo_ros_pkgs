ab93c5955918320fbdbb72c0a4462bf6468bc59b
Aditya Pande <aditya050995@gmail.com>
2021-07-22 13:35:31 -0700

    added set_parameter method based on ros2param call_set_parameter api
    Signed-off-by: Aditya Pande <aditya050995@gmail.com>

--- gazebo_ros/test/test_disable_performance_metrics.test.py
+++ gazebo_ros/test/test_disable_performance_metrics.test.py
@@ -26,14 +26,14 @@ import rclpy
 from gazebo_msgs.msg import PerformanceMetrics
 from rclpy.node import Node
 from rcl_interfaces.msg import Parameter, ParameterType
-from ros2param.api import call_set_parameters
+from rcl_interfaces.srv import SetParameters
 import time
 from threading import Thread
 
 
 @pytest.mark.launch_test
 def generate_test_description():
-    """ Launch a gzserver node  """
+    """Launch a gzserver node."""
     os.chdir(os.path.dirname(__file__))
     return launch.LaunchDescription([
         launch.actions.ExecuteProcess(
@@ -45,6 +45,7 @@ def generate_test_description():
         launch_testing.actions.ReadyToTest()
     ])
 
+
 class TestPerformanceMetricsParam_disable(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -55,17 +56,22 @@ class TestPerformanceMetricsParam_disable(unittest.TestCase):
         rclpy.shutdown()
 
     def test_parameter_disable(self):
-        """ Checks if /performance_metrics topic stops publishing if enable_performance_metrics is set to False  """
+        """Checks if /performance_metrics topic stops publishing if\
+        enable_performance_metrics parameter is set to False."""
         node = MakeTestNode('performance_metrics_disable_testing_node')
-        assert node.wait_for_gazebo_node(timeout_sec = 5.0), 'Gazebo node not found, parameter could not be set'
+
+        assert node.wait_for_gazebo_node(timeout_sec=5.0),\
+            'Gazebo node not found, parameter could not be set'
 
         # Set enable_performance_metrics parameter to False and listen for messages
-        response = node.set_parameter(False)
+        response = node.set_parameter(state=False)
         assert response.successful, 'Parameter could not be set to False'
 
         node.start_subscriber()
         time.sleep(2.5)
-        assert node.msg_count == 0, f'Received {node.msg_count} messages after setting enable_performance_metrics parameter to False, test failed'
+        assert node.msg_count == 0, f'Received {node.msg_count} messages after\
+            setting enable_performance_metrics parameter to False, test failed'
+
 
 class TestPerformanceMetricsParam_enable(unittest.TestCase):
     @classmethod
@@ -77,27 +83,32 @@ class TestPerformanceMetricsParam_enable(unittest.TestCase):
         rclpy.shutdown()
 
     def test_parameter_enable(self):
-        """ Checks if /performance_metrics topic starts publishing if enable_performance_metrics is set to True  """
+        """Checks if /performance_metrics topic starts publishing if\
+        enable_performance_metrics is set to True."""
         node = MakeTestNode('performance_metrics_enable_testing_node')
-        assert node.wait_for_gazebo_node(timeout_sec = 5.0), 'Gazebo node not found, parameter could not be set'
+
+        assert node.wait_for_gazebo_node(timeout_sec=5.0),\
+            'Gazebo node not found, parameter could not be set'
 
         # Set enable_performance_metrics parameter to True and listen for messages
-        response = node.set_parameter(True)
+        response = node.set_parameter(state=True)
         assert response.successful, 'Parameter could not be set to True'
 
         node.start_subscriber()
         time.sleep(2.5)
-        assert node.msg_count != 0, 'Did not receive any messages after setting enable_performance_metrics to True, test failed'
+        assert node.msg_count != 0, 'Did not receive any messages after\
+            setting enable_performance_metrics to True, test failed'
+
 
 class MakeTestNode(Node):
     def __init__(self, name):
-        """ Initialize node and counters  """
+        """Initialize node and counters."""
         super().__init__(name)
         self.flag_gazebo_node = False
         self.msg_count = 0
 
-    def wait_for_gazebo_node(self, timeout_sec = 5.0):
-        """ Wait for 'timeout_sec' seconds to detect a running gazebo node """
+    def wait_for_gazebo_node(self, timeout_sec=5.0):
+        """Wait for 'timeout_sec' seconds to detect a running gazebo node."""
         start = time.time()
         print('Waiting to find Gazebo Node...')
         while time.time() - start < timeout_sec and not self.flag_gazebo_node:
@@ -108,28 +119,42 @@ class MakeTestNode(Node):
             print('Gazebo node was not found !')
         return self.flag_gazebo_node
 
-    def set_parameter(self, state=False):
-        """ Set enable_performance_metrics parameter to boolean 'state' """
+    def set_parameter(self, state=False, timeout_sec=5.0):
+        """Set enable_performance_metrics parameter to boolean 'state'."""
         parameter = Parameter()
         parameter.name = 'enable_performance_metrics'
         parameter.value.bool_value = state
         parameter.value.type = ParameterType.PARAMETER_BOOL
 
-        return call_set_parameters(node=self, node_name='gazebo',
-                                    parameters=[parameter]).results[0]
+        client = self.create_client(SetParameters, 'gazebo/set_parameters')
+        ready = client.wait_for_service(timeout_sec)
+        if not ready:
+            raise RuntimeError('Wait for service timed out')
+
+        request = SetParameters.Request()
+        request.parameters = [parameter]
+        future = client.call_async(request)
+        rclpy.spin_until_future_complete(self, future)
+
+        response = future.result()
+        if response is None:
+            raise RuntimeError('Exception while setting parameter: {future.exception()}')
+
+        return response.results[0]
 
     def start_subscriber(self):
-        """ Start listening for messages on /performance_metrics topic, run rclpy.spin() on a separate thread  """
+        """Start listening for messages on /performance_metrics topic,
+        run rclpy.spin() on a separate thread."""
         self.subscription = self.create_subscription(
             PerformanceMetrics,
             'performance_metrics',
             self.subscriber_callback,
             10)
-        
+
         # Add a spin thread
         self.ros_spin_thread = Thread(target=lambda node: rclpy.spin(node), args=(self,))
         self.ros_spin_thread.start()
 
     def subscriber_callback(self, data):
-        """ Maintain a count of the messages received since self.start_subscriber() ran  """
+        """Maintain a count of the messages received since self.start_subscriber() ran."""
         self.msg_count += 1
