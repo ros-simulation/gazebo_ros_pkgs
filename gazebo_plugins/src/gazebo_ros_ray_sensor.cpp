aa564a79f19cedb2f883337925634791461d2a21
Pradheep-office <padhupradheep@gmail.com>
2021-11-03 15:47:05 +0100

    update for ray plugin, adding the forgotten if

--- gazebo_plugins/src/gazebo_ros_ray_sensor.cpp
+++ gazebo_plugins/src/gazebo_ros_ray_sensor.cpp
@@ -111,7 +111,11 @@ void GazeboRosRaySensor::Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementPt
 
   impl_->multi_robot_namespace_divider = impl_->ros_node_->get_namespace();
   impl_->multi_robot_namespace_divider.erase(0, 1);
-  impl_->multi_robot_namespace_divider += "/";
+
+  // Check if the namespace is empty
+  if (impl_->multi_robot_namespace_divider != "") {
+    impl_->multi_robot_namespace_divider += "/";
+  }
 
   // Get QoS profile for the publisher
   rclcpp::QoS pub_qos = qos.get_publisher_qos("~/out", rclcpp::SensorDataQoS().reliable());
