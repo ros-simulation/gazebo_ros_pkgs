b406c96377637c6de8944f01c801c519f1647daa
Aditya Pande <aditya050995@gmail.com>
2021-11-17 14:21:47 -0800

    Descriptive comments, world file cleanup
    Signed-off-by: Aditya Pande <aditya050995@gmail.com>

--- gazebo_plugins/test/test_gazebo_ros_wheelslip.cpp
+++ gazebo_plugins/test/test_gazebo_ros_wheelslip.cpp
@@ -56,18 +56,47 @@ public:
     for (auto & element : parameter_pairs) {
       parameter_names.push_back(element.first);
     }
-    std::this_thread::sleep_for(1s);
-    auto parameters_received = parameters_client->get_parameters(parameter_names);
-    for (auto & parameter : parameters_received) {
-      ASSERT_EQ(parameter_pairs[parameter.get_name()], parameter.as_double());
+
+    /* Query for the parameters and check if they match with the expected ones.
+       Plugin takes a while to set the parameters, so we make multiple attempts to
+       check the parameters */
+    int max_attempts = 5;
+    bool flag_do_parameters_match;
+    for (int i = 0; i < max_attempts; i++) {
+      // Assume all expected and received parameters match
+      flag_do_parameters_match = true;
+
+      // Query for parameters
+      auto parameters_received = parameters_client->get_parameters(parameter_names);
+      for (auto & parameter : parameters_received) {
+        if (parameter_pairs[parameter.get_name()] != parameter.as_double()) {
+          // Parameters don't match
+          flag_do_parameters_match = false;
+          break;
+        }
+      }
+
+      // Check the flag_do_parameters_match, if it was true, no need to query again
+      if (flag_do_parameters_match) {break;}
+      std::this_thread::sleep_for(0.2s);
     }
+    ASSERT_TRUE(flag_do_parameters_match);
   }
 };
 
 
-TEST_F(GazeboRosWheelSlipTest, TestWorldFile_2)
+TEST_F(GazeboRosWheelSlipTest, TestRosGlobalParamsOverrideSdf)
 {
-  // We have global ROS parameters and sdf parameters in the world file
+  // World file contains:
+  // - ROS parameters (for all wheels) - ('slip_compliance_unitless_lateral',
+  //                  'slip_compliance_unitless_longitudinal')
+  // - SDF tags for wheels : <slip_compliance_lateral>, <slip_compliance_longitudinal>
+  // ----------------------------------------------------------------------------------------------
+  // World file does NOT contain:
+  // - ROS Parameters (for each wheel) - ('slip_compliance_unitless_lateral/wheel_*',
+  //                  'slip_compliance_unitless_longitudinal/wheel_*')
+  // ----------------------------------------------------------------------------------------------
+  // Expected result: ROS parameters (for all wheels) override the ones in SDF
   std::map<std::string, double> parameter_pairs = {
     {"slip_compliance_unitless_lateral", 10.0},
     {"slip_compliance_unitless_lateral/wheel_front", 10.0},
@@ -81,9 +110,18 @@ TEST_F(GazeboRosWheelSlipTest, TestWorldFile_2)
   );
 }
 
-TEST_F(GazeboRosWheelSlipTest, TestWorldFile_3)
+TEST_F(GazeboRosWheelSlipTest, TestRosGlobalParamsApply)
 {
-  // We have only global ROS parameters in the world file
+  // World file contains:
+  // - ROS parameters ('slip_compliance_unitless_lateral',
+  //                  'slip_compliance_unitless_longitudinal')
+  // ----------------------------------------------------------------------------------------------
+  // World file does NOT contain:
+  // - SDF tags for wheels : <slip_compliance_lateral>, <slip_compliance_longitudinal>
+  // - ROS Parameters ('slip_compliance_unitless_lateral/wheel_*',
+  //                  'slip_compliance_unitless_longitudinal/wheel_*')
+  // ----------------------------------------------------------------------------------------------
+  // Expected result: ROS parameters (for all wheels) should apply
   std::map<std::string, double> parameter_pairs = {
     {"slip_compliance_unitless_lateral", 10.0},
     {"slip_compliance_unitless_lateral/wheel_front", 10.0},
@@ -98,9 +136,18 @@ TEST_F(GazeboRosWheelSlipTest, TestWorldFile_3)
 }
 
 
-TEST_F(GazeboRosWheelSlipTest, TestWorldFile_4)
+TEST_F(GazeboRosWheelSlipTest, RosLocalParamsOverrideSdf)
 {
-  // We have local ROS parameters and sdf parameters in the world file
+  // World file contains:
+  // - SDF tags for wheels : <slip_compliance_lateral>, <slip_compliance_longitudinal>
+  // - ROS Parameters (for each wheel) - ('slip_compliance_unitless_lateral/wheel_*',
+  //                  'slip_compliance_unitless_longitudinal/wheel_*')
+  // ----------------------------------------------------------------------------------------------
+  // World file does NOT contain:
+  // - ROS parameters (for all wheels) - ('slip_compliance_unitless_lateral',
+  //                  'slip_compliance_unitless_longitudinal')
+  // ----------------------------------------------------------------------------------------------
+  // Expected result: ROS parameters (for each wheel) should override the SDF ones
   std::map<std::string, double> parameter_pairs = {
     {"slip_compliance_unitless_lateral", 1.0},
     {"slip_compliance_unitless_lateral/wheel_front", 10.0},
@@ -114,9 +161,17 @@ TEST_F(GazeboRosWheelSlipTest, TestWorldFile_4)
   );
 }
 
-TEST_F(GazeboRosWheelSlipTest, TestWorldFile_5)
+TEST_F(GazeboRosWheelSlipTest, TestRosGloabalParamsOverrideAll)
 {
-  // We have global, local ROS params as well as sdf params in the world file
+  // World file contains:
+  // - SDF tags for wheels : <slip_compliance_lateral>, <slip_compliance_longitudinal>
+  // - ROS Parameters (for each wheel) - ('slip_compliance_unitless_lateral/wheel_*',
+  //                  'slip_compliance_unitless_longitudinal/wheel_*')
+  // - ROS parameters (for all wheels) - ('slip_compliance_unitless_lateral',
+  //                  'slip_compliance_unitless_longitudinal')
+  // ----------------------------------------------------------------------------------------------
+  // Expected result: ROS parameters (for all wheels) should override all SDF
+  // and ROS parameters (for each wheel)
   std::map<std::string, double> parameter_pairs = {
     {"slip_compliance_unitless_lateral", 100.5},
     {"slip_compliance_unitless_lateral/wheel_rear_left", 100.5},
@@ -133,8 +188,18 @@ TEST_F(GazeboRosWheelSlipTest, TestWorldFile_5)
 }
 
 
-TEST_F(GazeboRosWheelSlipTest, TestWorldFile_1)
+TEST_F(GazeboRosWheelSlipTest, TestSetParameters)
 {
+  // World file contains:
+  // - SDF tags for wheels : <slip_compliance_lateral>, <slip_compliance_longitudinal>
+  // ----------------------------------------------------------------------------------------------
+  // World file does NOT contain:
+  // - ROS Parameters (for each wheel) - ('slip_compliance_unitless_lateral/wheel_*',
+  //                  'slip_compliance_unitless_longitudinal/wheel_*')
+  // - ROS parameters (for all wheels) - ('slip_compliance_unitless_lateral',
+  //                  'slip_compliance_unitless_longitudinal')
+  // ----------------------------------------------------------------------------------------------
+  // Expected result: SDF parameters should apply to wheels
   this->Load("worlds/wheelslip_worlds/gazebo_ros_wheelslip_1.world", true);
   auto world = gazebo::physics::get_world();
   ASSERT_NE(nullptr, world);
