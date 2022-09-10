3eab7c10106724fe12b4d9d30802c276dfc2a271
Jacob Perron <jacob@openrobotics.org>
2022-09-09 17:07:14 -0700

    Add test to check output
    We expect some output on stderr when passing the 'verbose' argument.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- gazebo_ros/test/test_launch.py
+++ gazebo_ros/test/test_launch.py
@@ -13,10 +13,14 @@
 # limitations under the License.
 
 import os
+from threading import Event
 
 from ament_index_python.packages import get_package_share_path
 import launch
-from launch.actions.include_launch_description import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.actions import SetLaunchConfiguration
+from launch.event_handlers import OnProcessIO
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import launch_pytest
 import psutil
@@ -55,6 +59,39 @@ def launch_gzserver(gzserver_proc):
     ])
 
 
+@pytest.fixture()
+def launch_capture_io():
+
+    class LaunchCaptureIO:
+
+        def __init__(self):
+            self._event = Event()
+            self.stderr = []
+            self.action = RegisterEventHandler(OnProcessIO(on_stderr=self._on_stderr))
+
+        def _on_stderr(self, io):
+            self.stderr.append(io.text.decode('utf-8'))
+            self._event.set()
+
+        def wait_for_stderr(self, *, timeout) -> bool:
+            result = self._event.wait(timeout)
+            self._event.clear()
+            return result
+
+    return LaunchCaptureIO()
+
+
+@launch_pytest.fixture()
+def launch_gzserver_with_args(gzserver_proc, launch_capture_io):
+    return launch.LaunchDescription([
+        launch_capture_io.action,
+        SetLaunchConfiguration('physics', 'ode'),
+        SetLaunchConfiguration('verbose', 'true'),
+        gzserver_proc,
+        launch_pytest.actions.ReadyToTest()
+    ])
+
+
 @launch_pytest.fixture()
 def launch_gzclient(gzclient_proc):
     return launch.LaunchDescription([
@@ -76,6 +113,21 @@ def test_launch_gzserver(gzserver_proc, launch_context):
     current_process = psutil.Process()
     children = current_process.children(recursive=True)
     assert 'gzserver' in [i.name() for i in children], 'Error; gzserver not launched'
+
+
+@pytest.mark.launch(fixture=launch_gzserver_with_args)
+def test_launch_gzserver_with_args(gzserver_proc, launch_capture_io, launch_context):
+    current_process = psutil.Process()
+    children = current_process.children(recursive=True)
+    assert 'gzserver' in [i.name() for i in children], 'Error; gzserver not launched'
+
+    assert launch_capture_io.wait_for_stderr(timeout=5.0), \
+        'Expected output on stderr, received none'
+    assert 'Gazebo multi-robot simulator, version 11.11.0\nCopyright (C) 2012 Open Source ' \
+           'Robotics Foundation.\nReleased under the Apache 2 License.\nhttp://gazebosim.org\n\n' \
+           in launch_capture_io.stderr
+    assert not launch_capture_io.wait_for_stderr(timeout=1.0), \
+        f'Expected no more output on stderr, got {launch_capture_io.stderr}'
 
 
 @pytest.mark.launch(fixture=launch_gzclient)
